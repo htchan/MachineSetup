53421679b135d16c70d047dd61e579c815386b41
htchan <jacky19980619@gmail.com>
2025-04-20 14:55:03 +0800

    fix: git goto

--- scripts/git-setup.sh
+++ scripts/git-setup.sh
@@ -5,6 +5,6 @@ git config --global alias.a "add"
 git config --global alias.alias "config --get-regex ^alias"
 git config --global alias.unstage "restore --staged"
 git config --global alias.ck "checkout"
-git config --global alias.goto "!git branch -D $1; git checkout $1"
+git config --global alias.goto '!f() { git checkout origin/"$1" && (git branch -D "$1" 2>/dev/null || true) && git checkout "$1"; }; f'
 git config --global alias.diff-staged "diff --staged"
 git config --global alias.cp 'cherry-pick'
