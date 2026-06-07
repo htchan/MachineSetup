2479e09afe52683c7fcf3fd9bc6489f73e6ee9ba
htchan <jacky19980619@gmail.com>
2026-06-08 01:04:59 +0800

    feat: add free-gpu-memory alias when vulkan llama cpp hold the memory even stopped

--- scripts-fish/basic-alias.fish
+++ scripts-fish/basic-alias.fish
@@ -3,3 +3,5 @@ alias la='ls -A'
 alias ll='ls -alF'
 alias ls='ls --color=auto'
 alias start='xdg-open'
+# this works when vulkan llama cpp stopped but unable to released the memory.
+alias free-gpu-memory='sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches'
